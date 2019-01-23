18940ac3c2c8e4f0c2f6640e6bf5bc5efa19c4c1
Oliver <oliver.grant@gmail.com>
2019-01-23 23:27:21 +0000

    Done.

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,7 +5,9 @@
 # }
 
 def happy_birthday(birthday_kids)
-  # add your code snippet here!
+  birthday_kids.each do |name,age|
+    print "Happy Birthday #{name}! You are now #{age} years old!\n"
+  end
 end
 
 
